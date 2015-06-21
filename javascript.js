56a9abf3fc158370217b6c0dee3e57969427590a
krngrvr09 <krngrvr09@gmail.com>
2015-06-22 01:14:31 +0530

    added comments in the js code and made more readable

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function(){
-	
+
+	/**
+	* Colours the appropriate slot with the appropriate colour 
+	* using the parameters passed.
+	*
+	* @text: class name of the slot which is to be coloured
+	* @decorate: should it be coloured or not.
+	* #l_grey: if not coloured then should the colour be light grey
+	*          or dark grey.
+	**/
 	function colourIt(text,decorate,l_grey){
 		if(decorate){
 
@@ -21,8 +30,10 @@ $(document).ready(function(){
 			}
 	}
 
-	var slotSelected = [];
-	var value="";
+	var slotSelected = []; // Array of slots selected.
+	var value=""; // Stores the shoft form of the course most recently selected.
+	
+	// Hashmap with course name as key and time slots as value.
 	var courseToSlot={
 		"IMB": ["t1","f1"],
 		"MMBP":["t3","f3"],
@@ -79,6 +90,7 @@ $(document).ready(function(){
 		"CNT":["w1","f2"]
 	}
 
+	// Hashmap with time slot as key and courses in that slot as value
 	var slotToCourse = {
 		"m1":["DCS","PDCS","SWeb","LCS","NS","CMOS"],
 		"m2":["CMP","DMG","ML","CS","FCS","MCS"],
@@ -103,13 +115,15 @@ $(document).ready(function(){
 	}
 
 
-	var colour="#009933";
-	var colourArray=["#009933","#ff6600","#cc00cc","#0066ff","#9933ff"]
+	var colour="#009933"; // Highlight the slot with this colour
+	var colourArray=["#009933","#ff6600","#cc00cc","#0066ff","#9933ff"] // List of colours to choose from.
 	var light_grey = "#b2b2b2"
 	var dark_grey = "#4c4c4c"
-	courseSelected=[];
+	courseSelected=[]; // List of courses selected till now.
 	
-	var test={
+
+	// Maps time slot to a function which knows how to colour the slots.
+	var slotToFunction={
 		"m1" : function(decorate){
 					// console.log("m1");
 					colourIt("monday_first_slot",decorate, true);
@@ -182,9 +196,10 @@ $(document).ready(function(){
 	$(".course").click(function(){
 		
 		var value = $(this).attr("value");
-
+		// If the course is being checked, check if there are any conflicts,
+		// If not then colour that slot.
 		if($(this).is(":checked")){
-			// alert();
+			// Check for conflicts
 			for(var i=0;i<courseToSlot[value].length;i++){
 				
 				console.log("1 "+courseToSlot[value][i]);
@@ -196,37 +211,37 @@ $(document).ready(function(){
 				}
 				
 			}
+			// Add the course in courseSelected array
 			courseSelected.push(value);
+			// Add the slots to slotSelected Array
 			slotSelected = slotSelected.concat(courseToSlot[value]);
+			// Colour the slots
 			for( var i=0;i<slotSelected.length;i++){
 			
-				console.log(slotSelected[i]);
-				test[slotSelected[i]](true);
+				slotToFunction[slotSelected[i]](true);
 			}
-			console.log("course selected: "+courseSelected);
 		}
+		// If the course is being unchecked.
 		else{
 			
+			//Remove the slots from slotSelected array
 			slotSelected = slotSelected.filter( function( el ) {
   				return courseToSlot[value].indexOf( el ) < 0;
 			} );
 			
-			console.log("slot selected: "+slotSelected);
+			// Remove the course from courseselected.
 			if(courseSelected.indexOf(value)>-1){
 				courseSelected.splice(courseSelected.indexOf(value),1);
 				
 			}
 			
+			// Colour the slot
 			for( var i=0;i<courseToSlot[value].length;i++){
 					
-					test[courseToSlot[value][i]](false);
+					slotToFunction[courseToSlot[value][i]](false);
 			}
-		console.log(slotSelected[i]+" "+courseToSlot[value][i]);
 		
 		}
-
-		console.log(slotSelected);
-		
 
 	});
 
